c3769a47796f9f977bb4934dc9dbff083f1828dd
Im-suhyeon <00suhyen@gmail.com>
2023-06-02 20:52:58 +0900

    2300602 수현 userController.js 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,10 @@
-exports.signup = (req, res) => {
-  res.render("signup");
+const models = require("../models/index"),
+  Member = db.member;
+Op = models.Sequelize.Op;
+
+exports.signup = async (req, res) => {
+  // 회원가입 함수
+  models.res.render("signup");
 };
 
 exports.login = (req, res) => {
