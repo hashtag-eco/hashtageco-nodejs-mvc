38b157b253c5fe22e9dadcaf8f1d7b128dcd8c2f
Im-suhyeon <00suhyen@gmail.com>
2023-06-08 01:57:47 +0900

    230608 수현 product navbar 이미지 깨지는거 해결

--- public/js/signup.js
+++ public/js/signup.js
@@ -11,10 +11,6 @@ document.getElementById("signup-form").onsubmit = function () {
   let check = true;
 
 
-  //이메일 오류
-  if (email.includes("@")) {
-    let emailId = email.split("@")[0];
-    let emailServer = email.split("@")[1];
     //이메일 공백 오류
     if (emailId === "" || emailServer === "") {
       document.getElementById("emailError").innerHTML = "이메일을 입력해주세요";
@@ -23,13 +19,12 @@ document.getElementById("signup-form").onsubmit = function () {
     } else {
       document.getElementById("emailError").innerHTML = "";
     }
-  }
+  
   //이메일 @ 오류
-  else {
+  if {
     document.getElementById("emailError").innerHTML = "이메일이 올바르지 않습니다.";
     check = false;
-  }
-  // 이메일 중복 오류
+  };
 
   //이름 공백 오류
   if (name === "") {
@@ -47,15 +42,6 @@ document.getElementById("signup-form").onsubmit = function () {
     document.getElementById("nicknameError").innerHTML = "";
   }
 
-  //비밀번호 오류
-  // if (password !== passwordCheck) {
-  //   document.getElementById("passwordError").innerHTML = "";
-  //   document.getElementById("passwordCheckError").innerHTML = "비밀번호가 동일하지 않습니다.";
-  //   check = false;
-  // } else {
-  //   document.getElementById("passwordError").innerHTML = "";
-  //   document.getElementById("passwordCheckError").innerHTML = "";
-  // }
   //비밀번호 공백 오류
   if (password === "") {
     document.getElementById("passwordError").innerHTML = "비밀번호를 입력해주세요.";
